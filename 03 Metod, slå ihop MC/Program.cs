01c300ad5cbaee647987c8377e95f294368fdac0
Hannah J-H <h.jerminghavill@gmail.com>
2025-10-03 09:57:08 +0200

    Projekt 03: spånade lite mer på en MergeMC funktion. Har inte löst det än.

--- 03 Metod, slå ihop MC/Program.cs
+++ 03 Metod, slå ihop MC/Program.cs
@@ -4,28 +4,38 @@
 // Eventuella svårigheter: Se till att det bara blir 2 MC på samma plats,
 // alltså att programmet inte försöker parkera alla MC på samma plats
 
-Console.WriteLine("test");
+
+string vehicleType;
+string regNumber;
+string typeDivider = "#";
+string mcDivider = "|";
+string[] parkingSpaces = new string[101];       // Skapar 101 element (0-100). P-plats 0 ska aldrig användas --> 100 p-platser
+bool displayMenu = true;
+List<string> logs = new List<string>();     //skapar en lista för händelser
+
+// Lägger in en bil på parkingSpaces[0] - endast testdata. Vi tar bort det sen
+parkingSpaces[0] = "CAR#ABC123";
+parkingSpaces[1] = "MC#BBB222";
+parkingSpaces[2] = "MC#CCC333";
+parkingSpaces[3] = "MC#DDD444|MCEEE555";
 
 void MergeMC()
 {
-    for (int i = 0; i < vehicles.Length; i++)
+    for (int i = 0; i < parkingSpaces.Length; i++)
     {
-        if (vehicles[i].type != null)     // Här kanske man får ändra sen om type = MC
+        // OM det står en mc på platsen, men ingen mcDivider
+        // --> då står det 1 mc på platsen, inte 2
+        if (parkingSpaces[i].Contains("MC") && !(parkingSpaces[i].Contains("|")))     
         {
-            if (vehicles[i].type == "MC") // Får nog ska en ny egenskap, tex. bool .parkedtogether
-            {
-                if (vehicles[i].parkedtogher = false)
-                {
-                    // Här kan vi kanske använda metoderna Claes tipsade om i inlämningsuppgiften: 
-                    // String.Join String.Split
-                }
-            }
+            Console.WriteLine(parkingSpaces[i]); //Skriver ut alla mc som står själva
+            // Här borde vara kod för att slå ihop dem. Ska fundera på det sen.
             continue;
         }
         else
         {
-            VehicleRegistration(vehicles, i);   // Kanske borde returnera ett värde istället? 
-            break;
+            //ANNARS: hoppa vidare och kolla nästa p-plats
+            //(om det står 1 bil eller 2 mc på platsen kan vi strunta i p-platsen)
+            continue;
         }
 
     }
